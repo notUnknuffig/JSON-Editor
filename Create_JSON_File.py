915461025dee9013c12fc039b887ccec1d810880
notUnknuffig <139910467+notUnknuffig@users.noreply.github.com>
2024-03-29 22:10:24 +0100

    Alpha-1

--- Create_JSON_File.py
+++ Create_JSON_File.py
@@ -1,18 +1,14 @@
 import json
 import os
 
-class jsonObject:
-    def __init__(self):
-        self._json = "c"
-        self.layers = 0
-
-
-
-def getDeepestLayer(_json):
-    pass
+def fillData(_json):
+    return _json
 
 def parseStruct(_struct):
     i = 0
+    _struct = _struct.replace(" ", "")
+    _struct = _struct.replace("[", ":")
+    _struct = _struct.replace("{", ":")
     _struct = _struct.split(':')
     for char in range(len(_struct)): 
         _struct[char] = _struct[char].split('-')
@@ -33,14 +29,30 @@ def parseStruct(_struct):
             new_layer = '{'
             d_length = int(_struct[i][1])
             i += 1
-            for j in range(a_length):
-                new_layer += _struct[i][0] + ':'
-                
-        print(new_layer, i)
+            for j in range(d_length):
+                new_layer += '"' + _struct[i][0] + '":'
+                new_sub_layer = ""
+                if(_struct[i][1] == 'a'):
+                    new_sub_layer = '['
+                    sub_a_length = int(_struct[i][2])
+                    for jj in range(sub_a_length):
+                        new_sub_layer += '"r",'
+                    new_sub_layer = new_sub_layer[:-1]
+                    new_sub_layer += '],'
+                else:
+                    new_sub_layer = '"*",'
+                new_layer += new_sub_layer
+                i += 1
+            new_layer = new_layer[:-1]
+            new_layer += '}'
+        else: 
+            new_layer = '"*"'
         i += 1
         _json = _json.replace('c', new_layer)
-
-    return _json
+        _json = fillData(_json)
+    l_json = json.loads(_json)
+    return l_json
 
 if __name__ == '__main__':
-    print(parseStruct('a-10:d-3:key_a-*:key_b-*:key_b-*'))
+    _json = parseStruct(input())
+    print(_json)
--- Input_Operation.py
+++ Input_Operation.py
@@ -14,6 +14,7 @@ def main():
             file_name = input()
             file_path = create_path + "\\" + file_name + ".json"
             print(file_path)
+            new_file = None
             if(os.path.isfile(file_path)):
                 if(input('The File already exsists in the Directory. Do you want to Overwrite it instead? (y/n) ') == 'y'):
                     new_file = open(file_path, 'w')
@@ -23,16 +24,16 @@ def main():
             else:
                 new_file = open(file_path, 'x')
             print("""* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-                    *                                                                                       *
-                    *	How to Format a JSON-file:                                                          *
-                    *	* : creates an item that will be filled at the end of an operation.                 *
-                    *	a(n, item) : creates a new array with n indecies and fills it with items.           *
-                    *	d('key_a':item 'key_b':item) : creates a new dict with key as key and               *
-                    *                                  item as value.                                       *
-                    *                                                                                       *
-                    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *""")
+*                                                                                       *
+*   How to Format a Linear JSON-file:                                                   *
+*   * : creates an item that will be filled at the end of an operation.                 *
+*   a-n:item : creates a new array with n indecies and fills it with items.             *
+*   d-n:key_a-item:key_b-item : creates a new dict with key as key and item as value.   *
+*                                                                                       *
+*                                                                                       *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *""")
             file_struct = input('Enter file structur: ')
-            print(c_json_file.parseStruct(file_struct))
+            new_file.write(json.dumps(c_json_file.parseStruct(file_struct), indent=4))
         else:
             print('File-Error 1: Path entered is incorrect or not a directory.')
     elif(op_type == 'n'):
