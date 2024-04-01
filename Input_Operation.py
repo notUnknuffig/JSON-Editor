3016d06860d4a83ed5abbfedf9343ce6880f75c4
notUnknuffig <139910467+notUnknuffig@users.noreply.github.com>
2024-04-01 15:07:53 +0200

    Beta-0.2

--- Input_Operation.py
+++ Input_Operation.py
@@ -1,19 +1,19 @@
 import json
 import os
-import Create_JSON_File as c_json_file
-import Edit_JSON_File as e_json_file
+import json_file_operations as json_file_op
 
+possibleOperations = ['-show','-replace','-append', '-remove']
+newStructList = ['-replace', '-append']
 
 def main():
-    op_type = input('Do You want to create a new JSON-file? (y/n) ')
-    if(op_type == 'y'):
+    op_type = input('Do you want to Create a new JSON-File or edit an already exsisting one? (create/edit) ')
+    if(op_type == 'create'):
         print('Enter a path, where the JSON-file should be saved in.')
         create_path = input()
         if(os.path.isdir(create_path)):
             print('Enter a name for the file. (Do not enter a file extention!)')
             file_name = input()
             file_path = create_path + "\\" + file_name + ".json"
-            print(file_path)
             new_file = None
             if(os.path.isfile(file_path)):
                 if(input('The File already exsists in the Directory. Do you want to Overwrite it instead? (y/n) ') == 'y'):
@@ -23,27 +23,57 @@ def main():
                     return
             else:
                 new_file = open(file_path, 'x')
-            print('''* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*                                                                                               *
-*   How to Format a Linear JSON-file:                                                           *
-*   * : creates an item that will be filled at the end of an operation.                         *
-*   a,n[item] : creates a new array with n indecies and fills it with items.                    *
-*   d{(key, item), (key, (a,n[item]))} : creates a new dict with key as key and item as value.  *
-*                                                                                               *
-*                                                                                               *
-* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *''')
+            print('''----------------------------------------------------------
+How to Format a Linear JSON-file:
+   $ : creates an item that will be filled at the end of an operation.
+   a,n[item] : creates a new array with n indecies and fills it with items.
+   d{(key, item), (key, (a,n[item]))} : creates a new dict with key as key and item as value.
+----------------------------------------------------------''')
             file_struct = input('Enter file structur: ')
-            data = c_json_file.createStruct(c_json_file.parseStruct(file_struct))
+            data = json_file_op.createStruct(json_file_op.parseStruct(file_struct))
             new_file.write(json.dumps(data, indent=4))
+            new_file.close()
         else:
             print('File-Error 1: Path entered is incorrect or not a directory.')
-    elif(op_type == 'n'):
-        edit_path = input('Enter a path to the JSON-File')
+    elif(op_type == 'edit'):
+        edit_path = input('Enter a path to the JSON-File: ')
         if(os.path.isfile(edit_path)):
-            print('Opening File!')
+            with open(edit_path, 'r') as _file:
+                try:
+                    data = json.load(_file)
+                except json.decoder.JSONDecodeError:
+                    print('File-Error 3: JSON-Decode Error, File was loaded incorrectly or is empty.')
+                    return
+            print("""Enter a path to an element to edit:
+----------------------------------------------------------
+index: to select a specific item of an array,
+$: to select all items in an array,
+key: to select a specific value of a dict,
+#: to select all items in a dict.
+----------------------------------------------------------""")
+            path = input()
 
+            print("""Enter operation:
+-append: to add an element to an array or dict,
+-remove: to remove an element from an array or dict;""")
+            operator = input()
+
+            while operator not in possibleOperations:
+                print('Operation-Error 4: Operation entered Incorrectly.')
+                operator = input('Enter Operation again: ')
+            _struct = ""
+            if operator in newStructList:
+                print(f"""Enter a Structure to {operator} the selected element: """)
+                _struct = input()
+            operation = [operator, _struct]
+
+            data = json_file_op.getElement(path, data, operation)
+            with open(edit_path, 'w') as _file:
+               _file.write(json.dumps(data, indent=4))
+        else:
+            print('File-Error 2: Path entered is incorrect or not a file.')
     else:
         return
 
 if __name__ == '__main__':
-    main();
+    main()
---
+++ json_file_operations.py
@@ -0,0 +1,187 @@
+import json
+import os
+
+def replaceData(data, path, replaceChar):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            path.append(key)
+            if isinstance(value, str) and replaceChar in value:
+                _input = input(f"Enter replacement for '{value}' at {path}: ")
+                if len(_input) >= 0:
+                    _input = "/"
+                data[key] = value.replace(replaceChar, _input)
+                path.pop()
+            else:
+                data[key] = replaceData(value, path, replaceChar)
+        if len(path) > 1:
+            path.pop()
+    elif isinstance(data, list):
+        for i, item in enumerate(data):
+            path.append(i)
+            if isinstance(item, str) and replaceChar in item:
+                _input = input(f"Enter replacement for '{item}' at {path}: ")
+                if len(_input) >= 0:
+                    _input = "/"
+                data[i] = item.replace(replaceChar, _input)
+                path.pop()
+            else:
+                data[i] = replaceData(item, path, replaceChar)
+        if len(path) > 1:
+            path.pop()
+    return data
+
+
+def fillData(_json, replaceChar):
+    replaceData(_json, [], replaceChar)
+    return _json
+
+
+def parseStruct(_struct):
+    _struct = _struct.replace(" ", "")
+    nested_list = []
+    stack = []
+    current_string = ''
+
+    for char in _struct:
+        if char == '[' or char == '{' or char == '(':
+            if current_string:
+                nested_list.append(current_string)
+                current_string = ''
+            stack.append(nested_list)
+            nested_list = []
+        elif char == ']' or char == '}' or char == ')':
+            if current_string:
+                nested_list.append(current_string)
+                current_string = ''
+            if stack:
+                parent_list = stack.pop()
+                parent_list.append(nested_list)
+                nested_list = parent_list
+        elif char == ',':
+            if current_string:
+                nested_list.append(current_string)
+                current_string = ''
+        else:
+            current_string += char
+
+    if current_string:
+        nested_list.append(current_string)
+
+    return nested_list
+
+
+def createPartStruct(_struct):
+    part = ""
+    if _struct[0] == 'a':
+        part += '['
+        if str(_struct[1]).isnumeric():
+            for i in range(int(_struct[1])):
+                part += 'c,'
+            part = part[:-1]
+            part += ']'
+            part = part.replace('c', createPartStruct(_struct[2]))
+        else:
+            for item in _struct[1]:
+                part += createPartStruct(item) + ','
+            part = part[:-1]
+            part += ']'
+        
+    elif _struct[0] == 'd':
+        part += '{'
+        for key in _struct[1]:
+            part += '"' + key[0] + '":' + createPartStruct(key[1]) + ','
+        part = part[:-1]
+        part += '}'
+    elif _struct[0] == '"' and _struct[-1] == '"':
+        part = _struct
+    else:
+        part = '"$"'
+    return part
+
+
+def createStruct(_struct):
+    _json = createPartStruct(_struct)
+    l_json = json.loads(_json)
+    l_json = fillData(l_json, "$")
+    return l_json
+
+
+def executeOperation(operation, data):
+    if operation[0] == '-show':
+        print(data)
+    elif operation[0] == '-replace':
+        print(f"Replace {data} with: {operation[1]}")
+        if operation[1][0] == '#':
+            operation[1] = operation[1][1:]
+            data = createStruct(parseStruct(operation[1]))
+        else:
+            data = operation[1]
+    elif operation[0] == '-append':
+        if isinstance(data, list):
+            if operation[1][0] == '#':
+                data.append(createStruct(parseStruct(operation[1][1:])))
+            else:
+                data.append(operation[1])
+        elif isinstance(data, dict):
+            if operation[1][0] == '#':
+                new_data = parseStruct(operation[1][1:])
+                if isinstance(new_data[0], str):
+                    data[new_data[0]]  = createStruct(new_data[1])
+                else:
+                    data[input('Missing Key: Enter a key: ')] = createStruct(new_data)
+            elif operation[1][0] == '-':
+                _input = input(f'Enter Value for {[operation[1][1:]]} in {data}: ')
+                if len(_input) > 0: 
+                    data[operation[1][1:]] = _input
+                else:
+                    data[operation[1][1:]] = "/"
+        elif isinstance(data, str):
+            data += operation[1]
+    return data
+
+
+def getSubElement(_index, data, operation):
+    if _index == '':
+        data = executeOperation(operation, data)
+        return data
+    for i in range(len(_index)):
+        if str(_index[i]).isnumeric() and isinstance(data, list):
+            if(i > len(_index) - 1):
+                data[int(_index[i])] = executeOperation(operation, data[int(_index[i])])
+            else:
+                data[int(_index[i])] = getSubElement(_index, data[int(_index[i])], operation)
+
+        elif _index[i] == "$" and isinstance(data, list):
+            for j in range(len(data)):
+                if(i > len(_index) - 1):
+                    data[j] = executeOperation(operation, data[j])
+                else:
+                    data[j] = getSubElement(_index, data[j], operation)
+                
+        elif _index[i] in data and isinstance(data, dict):
+            if(i == len(_index) -1):
+                data[_index[i]] = executeOperation(operation, data[_index[i]])
+            else:
+                data[_index[i]] = getSubElement(_index, data[_index[i]], operation)
+
+        elif _index[i] == '#' and isinstance(data, dict):
+            keys = data.keys()
+            for key in keys:
+                if(i == len(_index) -1):
+                    data[key] = executeOperation(operation, data[key])
+                else:
+                    data[key] = getSubElement(_index, data[key], operation)
+    return data
+
+
+def getElement(_index, data, operation):
+    _index = _index.split(" ")
+    data = getSubElement(_index, data, operation)
+    return data
+
+
+if __name__ == '__main__':
+    edit_path = r'C:\Users\User\Documents\GitHub\JSON-Editor\test.json'
+    with open(edit_path, 'r') as _file:
+        data = json.load(_file)
+    print(getElement(input(), data, ['-append', '#key, (a["a","b","c"])']))
